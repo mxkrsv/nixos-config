f2eef3ffe8b5f4000041bdf7fffd38d53bcc8196
Maxim Karasev <mxkrsv@disroot.org>
2023-05-28 21:01:06 +0300

    system: use cpupower from linuxPackages_latest

--- system/configuration.nix
+++ system/configuration.nix
@@ -100,7 +100,7 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   environment.systemPackages = with pkgs; [
-    linuxKernel.packages.linux_6_2.cpupower
+    linuxPackages_latest.cpupower
     openssl
     pulsemixer
     htop
