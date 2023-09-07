8ccc5da08083cb686c192d382438c738559c5bd3
Maxim Karasev <mxkrsv@disroot.org>
2023-09-07 16:49:05 +0300

    system: nix: automatically optimize and gc

--- system/default.nix
+++ system/default.nix
@@ -10,6 +10,16 @@
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+
+    settings = {
+      auto-optimise-store = true;
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 
   # Pick only one of the below networking options.
