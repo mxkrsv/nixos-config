0f80d90d350b6874260b1a1b810f852468104afd
Maxim Karasev <mxkrsv@disroot.org>
2023-12-18 13:03:08 +0300

    system: homura: add nvidia driver (god forgive me)

--- system/homura/default.nix
+++ system/homura/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, config, lib, ... }: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -7,4 +7,36 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Nvidia
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "nvidia-x11"
+    "nvidia-settings"
+  ];
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      nvidiaBusId = "PCI:1:0:0";
+      intelBusId = "PCI:0:2:0";
+    };
+  };
+
+  # vaapi
+  hardware.opengl = {
+    extraPackages = with pkgs; [
+      intel-media-driver
+      libvdpau-va-gl
+    ];
+  };
 }
