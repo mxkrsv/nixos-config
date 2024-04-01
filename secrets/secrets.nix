8d779bd6d4c8fb6b106d3be5589064caeef32f25
Maxim Karasev <mxkrsv@disroot.org>
2024-04-01 21:05:06 +0300

    secrets: add homura user and host public keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,11 @@
 let
-  mxkrsv = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkupsms0fUp4SflgKwVBfKRXi8eY51cgbex4aXerC5B mxkrsv@sayaka";
-  users = [ mxkrsv ];
+  mxkrsv-sayaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkupsms0fUp4SflgKwVBfKRXi8eY51cgbex4aXerC5B mxkrsv@sayaka";
+  mxkrsv-homura = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpUBOdwjO4iYwQiPQ9vDtvolAas4rBzp/Bc2tv0Zk8h mxkrsv@homura";
+  users = [ mxkrsv-sayaka mxkrsv-homura ];
 
   sayaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPl3dGV0IV4p86Spql+QiZ6jEKqvD3XyY9c+wj3/yjZb";
-  systems = [ sayaka ];
+  homura = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbn7RG/45tR/xBncAYUkQeyh33qja4tdwlfWq5ROM+E";
+  systems = [ sayaka homura ];
 in
 {
   "gh_hosts.age".publicKeys = users;
