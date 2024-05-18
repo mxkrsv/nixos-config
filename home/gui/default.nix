f9c99ebbaf632fa27c93acac744a8eacf2574d48
Maxim Karasev <mxkrsv@disroot.org>
2024-05-18 11:52:20 +0300

    home: kanshi: profiles -> settings

--- home/gui/default.nix
+++ home/gui/default.nix
@@ -77,33 +77,40 @@
 
   services.kanshi = {
     enable = true;
-    profiles = {
-      undocked = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            status = "enable";
-          }
-        ];
-      };
-      docked = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            position = "3840,0";
-            status = "disable";
-          }
-          {
-            criteria = "BNQ BenQ GW2480 L1M0533101Q";
-            position = "1920,0";
-          }
-          {
-            criteria = "BNQ BenQ BL2480 H1J0087401Q";
-            position = "0,0";
-          }
-        ];
-      };
-    };
+
+    settings = [
+      {
+        profile = {
+          name = "undocked";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "docked";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              position = "3840,0";
+              status = "disable";
+            }
+            {
+              criteria = "BNQ BenQ GW2480 L1M0533101Q";
+              position = "1920,0";
+            }
+            {
+              criteria = "BNQ BenQ BL2480 H1J0087401Q";
+              position = "0,0";
+            }
+          ];
+        };
+      }
+    ];
   };
 
   programs.zathura = {
