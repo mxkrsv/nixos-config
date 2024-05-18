845ca4009968aed8d6703132aa3872af2ce954c7
Maxim Karasev <mxkrsv@disroot.org>
2024-05-18 11:44:39 +0300

    home: fastfetch: migrate from config file to newly added module

--- home/default.nix
+++ home/default.nix
@@ -27,7 +27,6 @@
   home.packages = with pkgs; [
     # text programs
     cmus
-    fastfetch
     texlive.combined.scheme-full
     libqalculate
     gnupg
@@ -90,13 +89,6 @@
     name = "Adwaita";
     size = 24;
     package = pkgs.gnome3.adwaita-icon-theme;
-  };
-
-  xdg.configFile = {
-    fastfetch = {
-      source = ./files/fastfetch.jsonc;
-      target = "fastfetch/config.jsonc";
-    };
   };
 
   programs.newsboat = {
@@ -351,4 +343,99 @@
 
   # E.g. for "command not found"
   programs.nix-index.enable = true;
+
+  programs.fastfetch = {
+    enable = true;
+
+    settings = {
+      logo = {
+        source = "nixos_old_small";
+        padding = {
+          top = 1;
+          left = 2;
+        };
+      };
+
+      display = {
+        separator = "";
+        keyWidth = 10;
+      };
+
+      modules = [
+        "title"
+        "break"
+        {
+          type = "os";
+          key = "os";
+          format = "{3}";
+        }
+        {
+          type = "host";
+          key = "host";
+        }
+        {
+          type = "kernel";
+          key = "kernel";
+        }
+        {
+          type = "uptime";
+          key = "uptime";
+        }
+        {
+          type = "packages";
+          key = "pkgs";
+        }
+        {
+          type = "shell";
+          key = "shell";
+        }
+        {
+          type = "display";
+          key = "video";
+        }
+        {
+          type = "de";
+          key = "de";
+        }
+        {
+          type = "wm";
+          key = "wm";
+          format = "{1}";
+        }
+        {
+          type = "terminal";
+          key = "term";
+        }
+        "break"
+        {
+          type = "cpu";
+          key = "cpu";
+          format = "{1}";
+        }
+        {
+          type = "gpu";
+          key = "gpu";
+          format = "{2}";
+        }
+        {
+          type = "memory";
+          key = "memory";
+          format = "{1} / {2}";
+        }
+        {
+          type = "disk";
+          key = "disk";
+          format = "{1} / {2} ({9})";
+        }
+        {
+          type = "battery";
+          key = "bat";
+        }
+        {
+          type = "poweradapter";
+          key = "pwr";
+        }
+      ];
+    };
+  };
 }
