43c72f3b9dc9006ca75a7dc8952fd201dc896080
Maxim Karasev <mxkrsv@disroot.org>
2024-05-20 18:38:58 +0300

    home: dconf: add default URI for virt-manager

--- home/gui/default.nix
+++ home/gui/default.nix
@@ -509,6 +509,11 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
+
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = [ "qemu:///system" ];
+      uris = [ "qemu:///system" ];
+    };
   };
 
   programs.chromium = {
