4280e1cd9d9ab61e1e325e41d46b9444e7c21fab
Maxim Karasev <mxkrsv@disroot.org>
2024-05-21 15:44:34 +0300

    home: don't setup default apps in the default module
    Leave it to the "sway" and "gnome" modules.

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -445,17 +445,17 @@
     ];
   };
 
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "application/pdf" = "org.pwmt.zathura.desktop";
-      "image/png" = "imv.desktop";
-      "image/jpeg" = "imv.desktop";
-      "text/html" = "org.qutebrowser.qutebrowser.desktop";
-      "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
-      "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
-    };
-  };
+  #xdg.mimeApps = {
+  #  enable = true;
+  #  defaultApplications = {
+  #    "application/pdf" = "org.pwmt.zathura.desktop";
+  #    "image/png" = "imv.desktop";
+  #    "image/jpeg" = "imv.desktop";
+  #    "text/html" = "org.qutebrowser.qutebrowser.desktop";
+  #    "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+  #    "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+  #  };
+  #};
 
   xdg.desktopEntries = {
     cmus = {
