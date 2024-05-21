78b568932ab6b0a4e9ba1621005d84ce90b87fc2
Maxim Karasev <mxkrsv@disroot.org>
2024-05-22 02:03:04 +0300

    home: gnome: configure epiphany search engines

--- home/gnome/apps/default.nix
+++ home/gnome/apps/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   home.packages = with pkgs; [
     # A task manager
     endeavour
@@ -14,5 +14,105 @@
       "x-scheme-handler/http" = "org.gnome.Epiphany.desktop";
       "x-scheme-handler/https" = "org.gnome.Epiphany.desktop";
     };
+  };
+
+  dconf.settings = {
+    # GNOME Web
+    "org/gnome/epiphany" =
+      {
+        search-engine-providers =
+          let
+            mkDictionaryEntry = lib.hm.gvariant.mkDictionaryEntry;
+            mkVariant = lib.hm.gvariant.mkVariant;
+          in
+          [
+            [
+              (mkDictionaryEntry [
+                "url"
+                (mkVariant "https://duckduckgo.com/?q=%s&t=epiphany")
+              ])
+              (mkDictionaryEntry [
+                "bang"
+                (mkVariant "!ddg")
+              ])
+              (mkDictionaryEntry [
+                "name"
+                (mkVariant "DuckDuckGo")
+              ])
+            ]
+            [
+              (mkDictionaryEntry [
+                "url"
+                (mkVariant "https://www.google.com/search?q=%s")
+              ])
+              (mkDictionaryEntry [
+                "bang"
+                (mkVariant "!g")
+              ])
+              (mkDictionaryEntry [
+                "name"
+                (mkVariant "Google")
+              ])
+            ]
+            [
+              (mkDictionaryEntry [
+                "url"
+                (mkVariant "https://context.reverso.net/translation/english-russian/%s")
+              ])
+              (mkDictionaryEntry [
+                "bang"
+                (mkVariant "!rc")
+              ])
+              (mkDictionaryEntry [
+                "name"
+                (mkVariant "ReversoContext")
+              ])
+            ]
+            [
+              (mkDictionaryEntry [
+                "url"
+                (mkVariant "https://search.nixos.org/packages?query=%s")
+              ])
+              (mkDictionaryEntry [
+                "bang"
+                (mkVariant "!nixos-packages")
+              ])
+              (mkDictionaryEntry [
+                "name"
+                (mkVariant "NixOS Packages")
+              ])
+            ]
+            [
+              (mkDictionaryEntry [
+                "url"
+                (mkVariant "https://search.nixos.org/options?query=%s")
+              ])
+              (mkDictionaryEntry [
+                "bang"
+                (mkVariant "!nixos-options")
+              ])
+              (mkDictionaryEntry [
+                "name"
+                (mkVariant "NixOS Options")
+              ])
+            ]
+            [
+              (mkDictionaryEntry [
+                "url"
+                (mkVariant "https://repology.org/projects/?search=%s")
+              ])
+              (mkDictionaryEntry [
+                "bang"
+                (mkVariant "!repology")
+              ])
+              (mkDictionaryEntry [
+                "name"
+                (mkVariant "Repology")
+              ])
+            ]
+          ];
+
+        default-search-engine = "Google";
+      };
   };
 }
