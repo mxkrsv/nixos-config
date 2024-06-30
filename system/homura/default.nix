d8b0a8c9452eb98c4cde739d0d9ca76d1c11e649
Maxim Karasev <mxkrsv@disroot.org>
2024-06-30 03:26:19 +0300

    system: homura: remove nvidia driver
    Switched to an AMD GPU.

--- system/homura/default.nix
+++ system/homura/default.nix
@@ -11,35 +11,10 @@
   # needed to unlock LUKS with key from TPM
   boot.initrd.systemd.enable = true;
 
-  # Nvidia
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "nvidia-x11"
-    "nvidia-settings"
-  ];
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      nvidiaBusId = "PCI:1:0:0";
-      intelBusId = "PCI:0:2:0";
-    };
-  };
-
   # vaapi
   hardware.opengl = {
     extraPackages = with pkgs; [
       intel-media-driver
-      libvdpau-va-gl
     ];
   };
 
