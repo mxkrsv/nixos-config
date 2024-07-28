6b76d55a6810dbdf039b16d73c1d572af663a7e2
Maxim Karasev <mxkrsv@disroot.org>
2024-07-28 12:48:14 +0300

    home: move foot from apps to sway/apps

--- home/sway/apps/default.nix
+++ home/sway/apps/default.nix
@@ -388,4 +388,44 @@
   };
 
   services.syncthing.tray.enable = true;
+
+  programs.foot = {
+    enable = true;
+    #server.enable = true; // can't execute desktop files in that case
+    settings = {
+      main = {
+        font = "JetBrains Mono:size=11";
+        dpi-aware = false;
+        pad = "0x0";
+      };
+      bell = {
+        urgent = true;
+      };
+      scrollback = {
+        lines = 8192;
+      };
+      # gruvbox
+      colors = {
+        background = "282828";
+        foreground = "ebdbb2";
+        regular0 = "282828";
+        regular1 = "cc241d";
+        regular2 = "98971a";
+        regular3 = "d79921";
+        regular4 = "458588";
+        regular5 = "b16286";
+        regular6 = "689d6a";
+        regular7 = "a89984";
+        bright0 = "928374";
+        bright1 = "fb4934";
+        bright2 = "b8bb26";
+        bright3 = "fabd2f";
+        bright4 = "83a598";
+        bright5 = "d3869b";
+        bright6 = "8ec07c";
+        bright7 = "ebdbb2";
+        alpha = "0.9";
+      };
+    };
+  };
 }
