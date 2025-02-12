93aa89f7502e85fbbb0e8a1fa5415f75daaa84a6
Maxim Karasev <mxkrsv@disroot.org>
2025-02-12 15:04:00 +0300

    system: sayaka: disable swapfile for now

--- system/sayaka/hardware-configuration.nix
+++ system/sayaka/hardware-configuration.nix
@@ -43,12 +43,12 @@
       fsType = "vfat";
     };
 
-  swapDevices = [
-    {
-      device = "/swap/swapfile";
-      size = 16 * 1024;
-    }
-  ];
+  #swapDevices = [
+  #  {
+  #    device = "/swap/swapfile";
+  #    size = 16 * 1024;
+  #  }
+  #];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
