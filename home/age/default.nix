a388611b75f157f9277c77cd19d940cd3cc5d8ee
Maxim Karasev <mxkrsv@disroot.org>
2025-02-12 15:02:24 +0300

    home: age: remove obsolete secrets

--- home/age/default.nix
+++ home/age/default.nix
@@ -1,14 +1,7 @@
 { config, ... }: {
   age = {
     secrets = {
-      gh_hosts = {
-        file = ../../secrets/gh_hosts.age;
-        path = "${config.xdg.configHome}/gh/hosts.yml";
-      };
-      glab_config = {
-        file = ../../secrets/glab_config.age;
-        path = "${config.xdg.configHome}/glab-cli/config.yml";
-      };
+      # Nothing here for now
     };
   };
 }
