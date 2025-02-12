473cadf6ec700da069b637e91471d3bb754b998c
Maxim Karasev <mxkrsv@disroot.org>
2025-02-12 14:58:43 +0300

    secrets: remove/replace obsolete keys and secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,12 @@
 let
-  mxkrsv-sayaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkupsms0fUp4SflgKwVBfKRXi8eY51cgbex4aXerC5B mxkrsv@sayaka";
-  mxkrsv-homura = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpUBOdwjO4iYwQiPQ9vDtvolAas4rBzp/Bc2tv0Zk8h mxkrsv@homura";
-  users = [ mxkrsv-sayaka mxkrsv-homura ];
+  poli-sayaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGZKFPQNRf1H/4k0wjCMiATRBQ1BoEYPSFX6EhGInWw poli@sayaka";
+  #poli-homura = "";
+  users = [ poli-sayaka ];
 
-  sayaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPl3dGV0IV4p86Spql+QiZ6jEKqvD3XyY9c+wj3/yjZb";
-  homura = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbn7RG/45tR/xBncAYUkQeyh33qja4tdwlfWq5ROM+E";
-  systems = [ sayaka homura ];
+  sayaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKFarrIP0OqMflQwktGYk084TzztWipdWtgMTPv5+cIR";
+  #homura = "";
+  systems = [ sayaka ];
 in
 {
-  "gh_hosts.age".publicKeys = users;
-  "glab_config.age".publicKeys = users;
-
-  "singbox_uuid_mxkrsv.age".publicKeys = users ++ systems;
+  "singbox_uuid.age".publicKeys = users ++ systems;
 }
