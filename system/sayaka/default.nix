2600352aa6bcd5de7be8a6bd79e8e7614e69dfa9
Maxim Karasev <mxkrsv@disroot.org>
2025-03-14 23:36:18 +0300

    system: sayaka: cleanup

--- system/sayaka/default.nix
+++ system/sayaka/default.nix
@@ -26,47 +26,6 @@
     enable = true;
   };
 
-  security.pam.services = {
-    #swaylock.text = ''
-    #  # Account management.
-    #  account required pam_unix.so
-
-    #  # Authentication management.
-    #  auth sufficient pam_unix.so   likeauth try_first_pass
-    #  auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
-    #  auth required pam_deny.so
-
-    #  # Password management.
-    #  password sufficient pam_unix.so nullok yescrypt
-
-    #  # Session management.
-    #  session required pam_env.so conffile=/etc/pam/environment readenv=0
-    #  session required pam_unix.so
-    #'';
-
-    ## slightly more security
-    #greetd.fprintAuth = false;
-    #login.fprintAuth = false;
-  };
-
-  #services.tlp = {
-  #  enable = true;
-  #  settings = {
-  #    CPU_SCALING_GOVERNOR_ON_AC = "performance";
-  #    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-  #    # amd-pstate=active
-  #    CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
-  #    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-  #    PLATFORM_PROFILE_ON_AC = "balanced";
-  #    PLATFORM_PROFILE_ON_BAT = "low-power";
-
-  #    RADEON_DPM_PERF_LEVEL_ON_AC = "auto";
-  #    RADEON_DPM_PERF_LEVEL_ON_BAT = "auto";
-  #  };
-  #};
-
   # lanzaboote
 
   # This should already be here from switching to bootspec earlier.
